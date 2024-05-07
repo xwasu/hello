526913382b0e4e811dea6e0053dfc51a0fc1142c
xwasu <paatryk.wasik@gmail.com>
2024-05-07 20:56:29 +0200

    Spring Boot

---
+++ src/test/java/io/github/xwasu/HelloWorldWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.xwasu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
