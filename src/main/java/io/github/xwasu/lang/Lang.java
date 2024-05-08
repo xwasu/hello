531be7de7ef1c8277dea41a01e5b8923464d43f0
xwasu <paatryk.wasik@gmail.com>
2024-05-08 08:17:06 +0200

    Transfer of language domain and Spring Data Repository

---
+++ src/main/java/io/github/xwasu/lang/Lang.java
@@ -0,0 +1,53 @@
+package io.github.xwasu.lang;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "languages")
+public class Lang {
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "inc", strategy = "increment")
+    private Integer id;
+    private String welcomeMsg;
+    private String code;
+
+    /**
+     * Hibernate (JPA) needs it.
+     */
+    @SuppressWarnings("unused")
+    public Lang() {
+
+    }
+
+    public Lang(Integer id, String welcomeMsg, String code) {
+        this.id = id;
+        this.welcomeMsg = welcomeMsg;
+        this.code = code;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getWelcomeMsg() {
+        return welcomeMsg;
+    }
+
+    public void setWelcomeMsg(String welcomeMsg) {
+        this.welcomeMsg = welcomeMsg;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
---
+++ src/main/java/io/github/xwasu/lang/LangRepository.java
@@ -0,0 +1,11 @@
+package io.github.xwasu.lang;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LangRepository extends JpaRepository<Lang, Integer> {
+    List<Lang> findByCodeAndWelcomeMsg(String code, String msg);
+}
